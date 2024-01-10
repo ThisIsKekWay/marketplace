5ba8b9b13a6cf75ca1be635b611748f86748014d
Koha <BelkoZdorovo@yandex.ru>
2024-01-10 12:03:50 +0300

    adaptive layout begining

--- Ecom/Ecom/settings.py
+++ Ecom/Ecom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)5#z6mkn6emavsle@awc+z+58-l2x))$7j_ip@jdk-yk2rrp17
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.109', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.4', 'localhost', '127.0.0.1']
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
